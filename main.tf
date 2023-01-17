8b623b213dcc1a3e383954fac1f52477b69e5828
Yauheniya Dubinina <Yauheniya.Dubinina@itechart-group.com>
2023-01-17 16:36:03 +0300

    add repository clone

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+data "template_file" "input" {
+  template=file("prep.sh")
+  vars ={
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+  user_data=base64encode(data.template_file.input.rendered)
+}
--- prep.sh
+++ prep.sh
@@ -1,2 +1,7 @@
+#!bin/sh
+git clone https://github.com/AltJane/flask_hello.git
+cd flask_hello
+apt-get update
+apt install python3-pip
 pip install -r requirements.txt
 python view.py
